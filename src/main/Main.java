0ab9031d055fe96142e2ce83a513a02448b9db9d
abhishek <abhishek.gupta1_cs18@gla.ac.in>
2020-02-23 17:47:18 +0530

    execution class

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main {
         LinkedList linkedList = new LinkedList();
         Methods functions = new Methods();
 
-        System.out.println("Hey there !! My name is Tushar Saxena " +
+        System.out.println("Hey there !! My name is Abhishek Gupta " +
                 ".. Here what can I do for you--->>");
         int c = 0;
         while (c != 5) {
